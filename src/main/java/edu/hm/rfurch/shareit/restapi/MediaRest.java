cbd1c153016f741a5197733bbb7ad04de1b163d5
Michael Schmid <m.schmid@hm.edu>
2017-05-01 22:27:02 +0200

    checksytle clean part 2

--- src/main/java/edu/hm/rfurch/shareit/restapi/MediaRest.java
+++ src/main/java/edu/hm/rfurch/shareit/restapi/MediaRest.java
@@ -27,61 +27,61 @@ import edu.hm.rfurch.shareit.model.Disc;
 @Path("/media")
 public class MediaRest {
     /**
-     * 
-     * @return
+     * JSON interface to get a list of all books.
+     * @return JSON response
      */
     @GET
     @Path("/books")
     @Produces(MediaType.APPLICATION_JSON)
     public Book[] getBooks() {
-    	List<Book> oBooks = new MediaService().getBooks().get().getResponseData().stream().filter(book -> book instanceof Book).map(book -> (Book)book).collect(Collectors.toList());
-    	Book[] books = new Book[oBooks.size()];
-    	oBooks.toArray(books);
+        List<Book> oBooks = new MediaService().getBooks().get().getResponseData().stream().filter(book -> book instanceof Book).map(book -> (Book)book).collect(Collectors.toList());
+        Book[] books = new Book[oBooks.size()];
+        oBooks.toArray(books);
         return books;
     }
 
     /**
-     * 
-     * @param isbn
-     * @return
+     * JSON interface to book.
+     * @param isbn of the book
+     * @return JSON response
      */
     @GET
     @Path("/books/{isbn}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBook(@PathParam("isbn") String isbn) {
-    	Response result = MediaServiceResult.BadRequest.getResponse();
+        Response result = MediaServiceResult.BadRequest.getResponse();
         if (isbn != null) {
-        	Optional<MediaServiceResult> oBook = new MediaService().getBook(isbn);
-        	result = oBook.isPresent() ? oBook.get().getResponse() : MediaServiceResult.BadRequest.getResponse();
+            Optional<MediaServiceResult> oBook = new MediaService().getBook(isbn);
+            result = oBook.isPresent() ? oBook.get().getResponse() : MediaServiceResult.BadRequest.getResponse();
         }
        return result;
     }
     
     
     /**
-     * 
-     * @param book
-     * @return
+     * JSON interface to add a book.
+     * @param book build by jackson
+     * @return JSON response
      */
     @POST
     @Path("/books")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addBook(Book book) {
-    	Response result;
-    	try {
-    		result = new MediaService().addBook(book).get().getResponse();
-    	} catch (NullPointerException exception) {
-			result = MediaServiceResult.BadRequest.getResponse();
-		}
-    	return result;
+        Response result;
+        try {
+            result = new MediaService().addBook(book).get().getResponse();
+        } catch (NullPointerException exception) {
+            result = MediaServiceResult.BadRequest.getResponse();
+        }
+        return result;
     }
     
     /**
-     * 
-     * @param book
-     * @param isbn
-     * @return
+     * JSON interface to update a book.
+     * @param book build by jackson
+     * @param isbn of the book
+     * @return JSON response
      */
     @PUT
     @Path("/books/{isbn}")
@@ -98,8 +98,8 @@ public class MediaRest {
     }
     
     /**
-     * 
-     * @return
+     * JSON interface to get a list of all discs.
+     * @return JSON response
      */
     @GET
     @Path("/discs")
@@ -109,64 +109,64 @@ public class MediaRest {
     }
     
     /**
-     * 
-     * @param barcode
-     * @return
+     * JSON interface to get a disc.
+     * @param barcode of the disc
+     * @return JSON response
      */
     @GET
     @Path("/discs/{barcode}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getDisc(@PathParam("barcode") String barcode) {
-    	Response result = MediaServiceResult.BadRequest.getResponse();
+        Response result = MediaServiceResult.BadRequest.getResponse();
         if (barcode != null) {
-        	Optional<MediaServiceResult> oBook = new MediaService().getDisc(barcode);
-        	result = oBook.isPresent() ? oBook.get().getResponse() : MediaServiceResult.BadRequest.getResponse();
+            Optional<MediaServiceResult> oBook = new MediaService().getDisc(barcode);
+            result = oBook.isPresent() ? oBook.get().getResponse() : MediaServiceResult.BadRequest.getResponse();
         }
        return result;
     }
     
     /**
-     * 
-     * @param disc
-     * @return
+     * JSON interface to add a disc.
+     * @param disc build by jackson
+     * @return JSON response
      */
     @POST
     @Path("/discs")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addDisc(Disc disc) {
-    	Response result;
-    	try {
-    		result = new MediaService().addDisc(disc).get().getResponse();
-    	} catch (NullPointerException exception) {
-			result = MediaServiceResult.BadRequest.getResponse();
-		}
-    	return result;
+        Response result;
+        try {
+            result = new MediaService().addDisc(disc).get().getResponse();
+        } catch (NullPointerException exception) {
+            result = MediaServiceResult.BadRequest.getResponse();
+        }
+        return result;
     }
     
     /**
-     * 
-     * @param disc
-     * @param barcode
-     * @return
+     * JSON interface to update a disc.
+     * @param disc build by jackson
+     * @param barcode of the disc
+     * @return JSON response
      */
     @PUT
     @Path("/discs/{barcode}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response updateDisc(Disc disc, @PathParam("barcode") String barcode) {
-    	Response result;
-    	if (disc != null && disc.getBarcode().equals(barcode)) {
-    		result = new MediaService().updateDisc(disc).get().getResponse();
-    	} else {
-			result = MediaServiceResult.BadRequest.getResponse();
-		}
-    	return result;
+        Response result;
+        if (disc != null && disc.getBarcode().equals(barcode)) {
+            result = new MediaService().updateDisc(disc).get().getResponse();
+        } else {
+            result = MediaServiceResult.BadRequest.getResponse();
+        }
+        return result;
     }
     
     /**
-     * 
-     * @return
+     * JSON interface to get a list of mediums.
+     * @return JSON response
      */
     @GET
     @Path("/mediums")
