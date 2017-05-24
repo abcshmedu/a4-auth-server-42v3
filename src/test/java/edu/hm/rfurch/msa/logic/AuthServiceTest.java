99d961110a5c8ecd001db7479754f75184837a68
Raphael Furch <raphael@familie-furch.de>
2017-05-24 20:25:05 +0200

    more tests

--- src/test/java/edu/hm/rfurch/msa/logic/AuthServiceTest.java
+++ src/test/java/edu/hm/rfurch/msa/logic/AuthServiceTest.java
@@ -12,27 +12,101 @@ import org.junit.Test;
 public class AuthServiceTest {
 
 
+
     @Test
-    public void proofTest(){
+    public void createNewUser(){
         ResourceManager.dataAccess().clear();
-        new AuthService().createNewUser("Michael", "Schmid", false);
-        String tokenV = new AuthService().getToken("Michael", "Schmid").getToken();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        Assert.assertEquals(400,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        Assert.assertEquals(400,new AuthService().createNewUser("Kevin", "Mueller", true).getCode());
+        Assert.assertEquals(400,new AuthService().createNewUser("Kevin", "Mueller2", false).getCode());
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin2", "Mueller", false).getCode());
+
+        Assert.assertEquals(400,new AuthService().createNewUser("", "Mueller", false).getCode());
+        Assert.assertEquals(400,new AuthService().createNewUser("Kevin2", "", false).getCode());
+        Assert.assertEquals(400,new AuthService().createNewUser(null, "Mueller", false).getCode());
+        Assert.assertEquals(400,new AuthService().createNewUser("Kevin2", null, false).getCode());
+    }
+
+    @Test
+    public void getTokenTestNot(){
+        ResourceManager.dataAccess().clear();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        Assert.assertEquals(400,new AuthService().getToken("Gevin", "Mueller").getCode());
+        Assert.assertEquals(400,new AuthService().getToken("Kevin", "Nueller").getCode());
+
+        Assert.assertEquals(400,new AuthService().getToken("", "Mueller").getCode());
+        Assert.assertEquals(400,new AuthService().getToken("Kevin", "").getCode());
+
+        Assert.assertEquals(400,new AuthService().getToken(null, "Mueller").getCode());
+        Assert.assertEquals(400,new AuthService().getToken("Kevin", null).getCode());
+        Assert.assertEquals(400,new AuthService().getToken(null, null).getCode());
+
+    }
+
+    @Test
+    public void getTokenTest(){
+        ResourceManager.dataAccess().clear();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        final String tokenValue = new AuthService().getToken("Kevin", "Mueller").getToken();
+        Assert.assertNotNull(tokenValue);
+        Assert.assertEquals(tokenValue, new AuthService().getToken("Kevin", "Mueller").getToken());
+
+    }
+
+
+    @Test
+    public void proofTestFalseFalse(){
+        ResourceManager.dataAccess().clear();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        String tokenV = new AuthService().getToken("Kevin", "Mueller").getToken();
         Assert.assertEquals(200,new AuthService().proofToken(tokenV, false).getCode());
     }
 
     @Test
-    public void proofTest2(){
+    public void proofTestTrueFalse(){
         ResourceManager.dataAccess().clear();
-        new AuthService().createNewUser("Michael", "Schmid", true);
-        String tokenV = new AuthService().getToken("Michael", "Schmid").getToken();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", true).getCode());
+        String tokenV = new AuthService().getToken("Kevin", "Mueller").getToken();
         Assert.assertEquals(200,new AuthService().proofToken(tokenV, false).getCode());
     }
 
     @Test
-    public void proofTest3(){
+    public void proofTestTrueTrue(){
         ResourceManager.dataAccess().clear();
-        new AuthService().createNewUser("Michael", "Schmid", true);
-        String tokenV = new AuthService().getToken("Michael", "Schmid").getToken();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", true).getCode());
+        String tokenV = new AuthService().getToken("Kevin", "Mueller").getToken();
         Assert.assertEquals(200,new AuthService().proofToken(tokenV, true).getCode());
+    }
+    @Test
+    public void proofTestFalseTrue(){
+        ResourceManager.dataAccess().clear();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        String tokenV = new AuthService().getToken("Kevin", "Mueller").getToken();
+        Assert.assertEquals(403,new AuthService().proofToken(tokenV, true).getCode());
+    }
+
+    @Test
+    public void proofTestWrongUser(){
+        ResourceManager.dataAccess().clear();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        String tokenV = new AuthService().getToken("Kevin2", "Mueller").getToken();
+        Assert.assertEquals(403,new AuthService().proofToken(tokenV, true).getCode());
+    }
+
+    @Test
+    public void proofTestWrongPw(){
+        ResourceManager.dataAccess().clear();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        String tokenV = new AuthService().getToken("Kevin", "Mueller2").getToken();
+        Assert.assertEquals(403,new AuthService().proofToken(tokenV, true).getCode());
+    }
+
+    @Test
+    public void proofTestAllWrong(){
+        ResourceManager.dataAccess().clear();
+        Assert.assertEquals(200,new AuthService().createNewUser("Kevin", "Mueller", false).getCode());
+        String tokenV = new AuthService().getToken("Kevin2", "Mueller2").getToken();
+        Assert.assertEquals(403,new AuthService().proofToken(tokenV, true).getCode());
     }
 }
