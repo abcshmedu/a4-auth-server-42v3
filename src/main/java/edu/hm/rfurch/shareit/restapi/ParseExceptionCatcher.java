7aa9df302e50ddd45b0adf27145290f025d4242e
Raphael Furch <raphael@familie-furch.de>
2017-05-31 21:28:12 +0200

    Catch parser exception works

--- src/main/java/edu/hm/rfurch/shareit/restapi/ParseExceptionCatcher.java
+++ src/main/java/edu/hm/rfurch/shareit/restapi/ParseExceptionCatcher.java
@@ -5,19 +5,16 @@ import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import edu.hm.rfurch.shareit.logic.MediaServiceResult;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 import java.util.Collection;
 
 /**
  * Created by Raphael Furch on 31/05/2017.
  */
-public class ConverterExceptionParser extends PropertyBindingException {
+public class ParseExceptionCatcher implements ExceptionMapper<PropertyBindingException> {
 
-    protected ConverterExceptionParser(String msg, JsonLocation loc, Class<?> referringClass, String propName, Collection<Object> propertyIds) {
-        super(msg, loc, referringClass, propName, propertyIds);
-    }
 
     public Response toResponse(PropertyBindingException exception){
-        System.out.println("test2");
         return MediaServiceResult.IamATeapot.getResponse();
     }
 
